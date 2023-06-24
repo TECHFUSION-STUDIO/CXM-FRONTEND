78a71fbab2aa392794c231fa6c649f58c0b844ac
Avi9631 <avikumarshooters@gmail.com>
2023-06-24 18:29:39 +0530

    g

---
+++ src/components/Screens/CreateProjectScreen.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="bg-white shadow shadow-sm mt-3 p-3">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Enter Project Name</label>
+      <input
+        type="text"
+        class="form-control"
+        id="exampleFormControlInput1"
+        v-model="projectName"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label"
+        >Project Description</label
+      >
+      <textarea
+        class="form-control"
+        id="exampleFormControlTextarea1"
+        rows="3"
+        v-model="projectDescription"
+      ></textarea>
+    </div>
+
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label"
+        >Project Visibility</label
+      >
+      <br />
+      <select
+        class="form-select"
+        aria-label="Default select example"
+        v-model="projectVisibility"
+      >
+        <option value="Public">Public</option>
+        <option value="Private">Private</option>
+      </select>
+    </div>
+
+    <div class="text-center">
+      <button class="btn btn-outline-danger m-2 w-25">Reset</button>
+      <button class="btn btn-outline-success m-2 w-25" @click="createProject()">
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axioscon from "../../axioscon.js";
+export default {
+  name: "CreateProjectScreen",
+  data() {
+    return {
+      axioscon,
+      projectName: "",
+      projectDescription: "",
+      projectVisibility: "",
+    };
+  },
+  methods: {
+    createProject() {
+      axioscon
+        .post("/createprojects", {
+          addedDateTime: "",
+          businessId: 1,
+          projectDescription: this.projectDescription,
+          projectName: this.projectName,
+          visibility: this.projectVisibility,
+        })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
